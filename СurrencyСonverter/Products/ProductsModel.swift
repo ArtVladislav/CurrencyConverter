de50086d17382c8d899bd371a800e5c74b3422f0
VladislavArtiukhov <severuswolf@icloud.com>
2025-03-16 22:31:22 +0300

    [CC - 2] - new Module Products on MVP

---
+++ СurrencyСonverter/Products/ProductsModel.swift
@@ -0,0 +1,11 @@
+//
+//  ProductsModel.swift
+//  СurrencyСonverter
+//
+//  Created by Владислав Артюхов on 16.03.2025.
+//
+
+struct ProductsModel {
+    let name: String
+    let countTransaction: Int
+}
